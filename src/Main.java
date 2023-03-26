bb9f4e260a37a93805ece8a0babd487a43e4e053
Anna Golubeva <agolubeva9483@gmail.com>
2023-03-26 17:57:14 +0300

    Added bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 25_572;
+        int mile = 20;
+
+        int bonus = ticket / mile;
+
+        System.out.println("Начиленные мили: " + bonus);
+    }
+}
